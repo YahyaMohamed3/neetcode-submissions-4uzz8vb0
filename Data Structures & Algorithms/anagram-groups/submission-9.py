c227a1e27ee56a579182a55ecdd5ea2e552f389c
Yahya Moahmed <154776983+YahyaMohamed3@users.noreply.github.com>
2026-04-04 07:56:26 -0400

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        base = ord("a")
+
+        for s in strs:
+            key = [0] * 26
+            for c in s:
+                key[ord(c) - base] += 1
+            res[tuple(key)].append(s)
+        return list(res.values())
+
+        
+            
+            
+        
