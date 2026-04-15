904356004340d5d134a7dc447a6460d79e115c92
Yahya Moahmed <154776983+YahyaMohamed3@users.noreply.github.com>
2026-04-15 02:19:35 -0400

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
@@ -0,0 +1,38 @@
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.isEndOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isEndOfWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(root, j):
+            cur = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(child, i + 1):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.isEndOfWord
+        return dfs(self.root, 0)
+
+                  
+        
