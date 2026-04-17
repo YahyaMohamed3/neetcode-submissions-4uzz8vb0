017bfa6a45aaf2607f07109e24f6af4c33e2f406
Yahya Moahmed <154776983+YahyaMohamed3@users.noreply.github.com>
2026-04-17 00:49:34 -0400

    Add: design-twitter-feed - submission-7

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-7.py
@@ -0,0 +1,49 @@
+import heapq
+from collections import defaultdict
+from typing import List
+
+class Twitter:
+
+    def __init__(self):
+        self.count = 0
+        self.tweets = defaultdict(list)      # userId -> [count, tweetId]
+        self.following = defaultdict(set)    # userId -> set of followeeIds
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append([self.count, tweetId])
+        self.count -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+        minHeap = []
+
+        self.following[userId].add(userId)
+        for followeeId in self.following[userId]:
+            if followeeId in self.tweets and self.tweets[followeeId]:
+                index = len(self.tweets[followeeId]) - 1
+                count, tweetId = self.tweets[followeeId][index]
+                minHeap.append([count, tweetId, followeeId, index - 1])
+
+        heapq.heapify(minHeap)
+        while minHeap and len(res) < 10:
+            count, tweetId, followeeId, index = heapq.heappop(minHeap)
+            res.append(tweetId)
+            if index >= 0:
+                count, tweetId = self.tweets[followeeId][index]
+                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
+        return res 
+
+
+
+
+
+
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId != followerId:
+            self.following[followerId].discard(followeeId)
