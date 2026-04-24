34e63d959b80d07c244b3dd9945e4bafc66c0032
Yahya Moahmed <154776983+YahyaMohamed3@users.noreply.github.com>
2026-04-23 23:25:52 -0400

    Add: islands-and-treasure - submission-17

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-17.py
@@ -0,0 +1,30 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        queue = deque()
+        directions = [[-1, 0] , [1, 0] , [0 , 1], [0 , -1]]
+
+        def addCell(r , c):
+            if (r not in range(rows) or c not in range(cols) 
+                or (r, c) in visited or grid[r][c] == -1):
+                return
+
+            visited.add((r, c))
+            queue.append((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    visited.add((r, c))
+                    queue.append((r, c))
+        
+        dist = 0
+        while queue:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+                grid[r][c] = dist
+                for dr , dc in directions:
+                    addCell(dr + r , dc + c)
+            dist+=1
