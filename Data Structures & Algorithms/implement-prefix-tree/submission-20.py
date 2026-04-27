300f7212623bd4d70db006a819b331264047c909
Yahya Moahmed <154776983+YahyaMohamed3@users.noreply.github.com>
2026-04-27 02:34:24 -0400

    Add: implement-prefix-tree - submission-20

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-20.py
@@ -0,0 +1,31 @@
+class PrefixTree:
+    def __init__(self):
+        self.children = {}
+        self.isEndOfWord = False
+    
+    def insert(self, word):
+        cur = self
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = PrefixTree()
+            cur = cur.children[c]
+        cur.isEndOfWord = True
+    
+    def search(self, word):
+        cur = self 
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.isEndOfWord
+    
+    def startsWith(self, prefix):
+        cur = self
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
